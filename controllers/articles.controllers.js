325973a98c67547bde445250952715a721df1b56
nicolgh <nicol.herta1@gmail.com>
2024-05-29 15:33:00 +0100

    completes DELETE /api/comments/:comment_id end point

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -3,7 +3,7 @@ const db = require('../db/connection')
 const articles = require('../db/data/test-data/articles')
 const comments = require('../db/data/test-data/comments')
 const articleData = require('../db/data/test-data/index')
-const {selectsArticles,selectArticleById,selectArticleComments,checkItemExistsInTable,createComment,calculateVotes} = require('../modules/articles.models')
+const {selectsArticles,selectArticleById,selectArticleComments,checkItemExistsInTable,createComment,calculateVotes, removeComment} = require('../modules/articles.models')
 
 exports.getArticleById = (req,res,next) => {
     const article_id = req.params.article_id
@@ -48,5 +48,13 @@ exports.updateVotes = (req,res,next) => {
     Promise.all(promises).then((resolvedPromises) => {
         const article = resolvedPromises[0]
         res.status(200).send({article})
+    }).catch(next)
+}
+
+exports.deleteComment = (req,res,next) => {
+    const {comment_id} = req.params
+    removeComment(comment_id).then((msg)=> {
+        console.log(msg,'msg')
+        res.status(204).send({msg})
     }).catch(next)
 }
