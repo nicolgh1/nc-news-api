9967ebc8ee877462999dd41d9dcc355f686e439a
nicolgh <nicol.herta1@gmail.com>
2024-05-29 10:26:58 +0100

    completes the end point GET /api/articles

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -2,6 +2,7 @@ const {
   convertTimestampToDate,
   createRef,
   formatComments,
+  matchArraysOnKey
 } = require("../db/seeds/utils");
 
 describe("convertTimestampToDate", () => {
--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 const {getInstructions,getTopics} = require('./controllers/topics.controllers')
-const {getArticleById} = require('./controllers/articles.controllers')
+const {getArticles,getArticleById} = require('./controllers/articles.controllers')
 
 app.get('/api',getInstructions)
 
 app.get('/api/topics', getTopics)
 
+app.get('/api/articles',getArticles)
 app.get('/api/articles/:article_id',getArticleById)
 
 app.use((err, req, res, next)=>{
