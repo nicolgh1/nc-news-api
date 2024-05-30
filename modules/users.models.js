c9af784754ac316593ba18a70c218ed480ea0c28
nicolgh <nicol.herta1@gmail.com>
2024-05-30 13:49:22 +0100

    completes GET /api/users/:username end point

--- modules/users.models.js
+++ modules/users.models.js
@@ -8,3 +8,15 @@ exports.selectUsers = () => {
         return response.rows
     })
 }
+exports.selectUserByUsername = (username) => {
+    return db.query(`
+    SELECT * FROM users WHERE username = $1`,[username]).then((response) => {
+        if(response.rows.length === 0){
+            return Promise.reject({
+                status: 404,
+                msg: 'Not Found'
+            })
+        }
+        return response.rows[0]
+    })
+}
